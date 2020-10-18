128f568e4bfd74319246ee7c1aa39b947266a5b2
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-18 21:11:07 +0600

    Alternate

--- main.c
+++ main.c
@@ -10,13 +10,14 @@ int main(void) {
 
     for (int i = 0; i < n; i++) {
 
-        for (int j = 0; j < n; j++) {
-
-            if (i+j < n - 1)
-                printf(" ");
-            else
-                printf("#");
+        for (int j = n-1; j >= i; j--) {
+            printf(" ");
         }
+
+        for (int k = 0; k <= i; k++) {
+            printf("#");
+        }
+
         printf("\n");
     }
 
