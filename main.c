5c598d16c784b0d2220cd44160f26c09c95f9125
ZarifAhsan <mdzarifahsan@gmail.com>
2020-10-18 20:59:29 +0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int n;
+    do {
+        printf("Height: ");
+        scanf("%d", &n);
+    } while (n <= 1 || n > 8);
+
+    for (int i = 0; i < n; i++) {
+
+        for (int j = 0; j < n; j++) {
+
+            if (i+j < n - 1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
